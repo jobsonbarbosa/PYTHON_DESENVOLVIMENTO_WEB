5149c06274c3b07ea7c408e9500c26db579d06e8
Jobson Barbosa <jobson.barbosa@gmail.com>
2024-10-16 14:32:03 -0300

    Adicionando dados em uma lista

--- FLASKWEB/app.py
+++ FLASKWEB/app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 produtos = []
+produtos_valores = []
 
 @app.route('/', methods=["GET", "POST"])
 def home():
@@ -10,18 +11,25 @@ def home():
     
     if request.method == "POST":
         if request.form.get("produtos"):
-            produtos.append(request.form.get("produtos"))        
+            if request.form.get("produtos") != "Jobson":
+                produtos.append(request.form.get("produtos"))
+            else:
+                produtos.append("Produto proibido")      
         
     return render_template("index.html", produtos=produtos)
   
-@app.route('/sobre')
+@app.route('/sobre', methods=["GET", "POST"])
 def sobre():
-    produtos = {
-        "Asfalto": 468.49,
-        "Emulsões": 1523.50,
-        "Aditivos": 856.90,
-        "CAP": 3456.10, 
-        "Asfalto Morno": 4560.00
-    }
-    return render_template("sobre.html", produtos=produtos)
+    # produtos = {
+    #     "Asfalto": 468.49,
+    #     "Emulsões": 1523.50,
+    #     "Aditivos": 856.90,
+    #     "CAP": 3456.10, 
+    #     "Asfalto Morno": 4560.00
+    # }
+    
+    if request.method== "POST":
+        if request.form.get("produto") and request.form.get("valor"):
+            produtos_valores.append({"produto":request.form.get("produto"), "valor":request.form.get("valor")})
+    return render_template("sobre.html", produtos_valores=produtos_valores)
 app.run(debug=True)
