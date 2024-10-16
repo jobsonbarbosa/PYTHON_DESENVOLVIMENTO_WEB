de102d8ea65bad5f1fa107a7cb72cb633b6a05ef
Jobson Barbosa <jobson.barbosa@gmail.com>
2024-10-16 13:46:53 -0300

    Adicionando dados em lista

--- FLASKWEB/app.py
+++ FLASKWEB/app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+produtos = []
+
+@app.route('/', methods=["GET", "POST"])
 def home():
-    produtos = ["Asfalto", "Emulsões", "Aditivos", "CAP", "Lama Asfáltica", "Pintura de Ligação"]
+    # produtos = ["Asfalto", "Emulsões", "Aditivos", "CAP", "Lama Asfáltica", "Pintura de Ligação"]
+    
+    if request.method == "POST":
+        if request.form.get("produtos"):
+            produtos.append(request.form.get("produtos"))        
         
     return render_template("index.html", produtos=produtos)
   
@@ -14,7 +20,8 @@ def sobre():
         "Asfalto": 468.49,
         "Emulsões": 1523.50,
         "Aditivos": 856.90,
-        "CAP": 3456.10 
+        "CAP": 3456.10, 
+        "Asfalto Morno": 4560.00
     }
     return render_template("sobre.html", produtos=produtos)
 app.run(debug=True)
